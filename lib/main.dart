7a4784f45a906688bca9b99633adf9ead37b120a
khedro <ahmedkhadr810@gmail.com>
2021-09-01 17:32:51 +0200

    edit some features

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products_provider.dart';
+import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/product_overview_screen.dart';
 
 void main() {
@@ -8,14 +11,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My shop',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.redAccent,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (_) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'My shop',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.redAccent,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (_) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   const ProductItem({
@@ -15,31 +16,38 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(12),
-      child: GridTile(
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+      child: GestureDetector(
+        onTap: () => Navigator.pushNamed(
+          context,
+          ProductDetailScreen.routeName,
+          arguments: id,
         ),
-        footer: GridTileBar(
-          leading: IconButton(
-            icon: Icon(
-              Icons.favorite,
-              color: Theme.of(context).accentColor,
+        child: GridTile(
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+          footer: GridTileBar(
+            leading: IconButton(
+              icon: Icon(
+                Icons.favorite,
+                color: Theme.of(context).accentColor,
+              ),
+              onPressed: () {},
             ),
-            onPressed: () {},
-          ),
-          trailing: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.shopping_cart_rounded,
-              color: Theme.of(context).accentColor,
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.shopping_cart_rounded,
+                color: Theme.of(context).accentColor,
+              ),
             ),
+            title: Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+            backgroundColor: Colors.black54,
           ),
-          title: Text(
-            title,
-            textAlign: TextAlign.center,
-          ),
-          backgroundColor: Colors.black54,
         ),
       ),
     );
