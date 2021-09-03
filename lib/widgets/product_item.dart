cee4c948a54ddd4528eeb7018b991274d6091a25
khedro <ahmedkhadr810@gmail.com>
2021-09-03 17:22:11 +0200

    add snackbar feature

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -33,11 +33,24 @@ class ProductItem extends StatelessWidget {
               ),
             ),
             trailing: IconButton(
-              onPressed: () => cart.addItem(
-                product.id,
-                product.title,
-                product.price,
-              ),
+              onPressed: () {
+                cart.addItem(
+                  product.id,
+                  product.title,
+                  product.price,
+                );
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: const Text('Add item to the cart.'),
+                    duration: Duration(seconds: 2),
+                    action: SnackBarAction(
+                      label: 'UNDO',
+                      onPressed: () => cart.removeSingleItem(product.id),
+                    ),
+                  ),
+                );
+              },
               icon: Icon(
                 Icons.shopping_cart_rounded,
                 color: Theme.of(context).accentColor,
