f26abe3571b93a6967b7e72c11bfda1b357341fd
khedro <ahmedkhadr810@gmail.com>
2021-09-03 19:13:00 +0200

    add app drawer to user products screen

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/models/products_provider.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/user_product_item.dart';
 
 class UserProductsScreen extends StatelessWidget {
@@ -13,6 +14,7 @@ class UserProductsScreen extends StatelessWidget {
     final products = Provider.of<ProductsProvider>(context).items;
     return Scaffold(
       backgroundColor: Color.fromRGBO(251, 247, 249, 0.9),
+      drawer: AppDrawer(),
       appBar: AppBar(
         title: const Text('Your Products'),
         actions: [
