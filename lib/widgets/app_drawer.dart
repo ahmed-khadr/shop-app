5c3f4dcf40e201c68e10f7475f09cd505831a53d
khedro <ahmedkhadr810@gmail.com>
2021-09-03 19:11:14 +0200

    add new page to app drawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/screens/order_screen.dart';
+import 'package:shop_app/screens/user_products_screen.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({Key? key}) : super(key: key);
@@ -17,14 +18,27 @@ class AppDrawer extends StatelessWidget {
             title: const Text('Shop'),
             onTap: () => Navigator.pushReplacementNamed(context, '/'),
           ),
-          Divider(),
+          const Divider(
+            color: Colors.black54,
+          ),
           ListTile(
             leading: Icon(Icons.payment),
             title: const Text('Orders'),
             onTap: () =>
                 Navigator.pushReplacementNamed(context, OrderScreen.routeName),
           ),
-          Divider(),
+          const Divider(
+            color: Colors.black54,
+          ),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: const Text('Manage Products'),
+            onTap: () => Navigator.pushReplacementNamed(
+                context, UserProductsScreen.routeName),
+          ),
+          const Divider(
+            color: Colors.black54,
+          ),
         ],
       ),
     );
